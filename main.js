6c9edc145bd9e44edcff2ba1d81ba7edbb08451c
Dante L'Erario <dantelerario@gmail.com>
2020-04-08 16:07:56 +0200

    done show/hide

---
+++ main.js
@@ -0,0 +1,9 @@
+//SHOW - HIDE Hamburger Menu
+
+$('.header-right > a').click( function() {
+  $('.hamburger-menu').show();
+});
+
+$('.hamburger-menu > a').click( function() {
+  $('.hamburger-menu').hide();
+});
